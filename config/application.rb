f3bd54104f05f2b5ab0c4067fb2753379e5c26ed
Nicollas Isabel <nicollasisabel@hotmail.com>
2019-03-16 14:12:32 -0300

    Configurações de timezone e idioma

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,14 @@ module DiscipulosDoPato
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
+    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+    config.time_zone = 'Brasilia'
+    # ActiveRecord Timezone
+    config.active_record.default_timezone = :local
+
+    # i18n
+    config.i18n.default_locale = 'pt-BR'
   end
 end
