2107d5da956c1457406b4a76fb80f90eedc3a950
Nicollas Isabel <nicollasisabel@hotmail.com>
2019-03-16 19:28:31 -0300

    Incrementando seed

--- db/seeds/plans.rb
+++ db/seeds/plans.rb
@@ -5,8 +5,10 @@ puts 'Removing all Plannings...'
 Planning.destroy_all
 puts 'Seeding Plannings...'
 
-ExpenditureClassification.all.each do |ec|
-  next if rand(10) < 5
+Rich.all.each do |rich|
+  ExpenditureClassification.all.each do |ec|
+    next if rand(10) < 5
 
-  Planning.create!([{ rich: Rich.last, expenditure_classification: ec, value: rand(0..500) }])
+    Planning.create!([{ rich: rich, expenditure_classification: ec, value: rand(0..500) }])
+  end
 end
--- db/seeds/riches.rb
+++ db/seeds/riches.rb
@@ -4,4 +4,5 @@
 puts 'Removing all Riches...'
 Rich.destroy_all
 puts 'Seeding Riches...'
-Rich.create!([{ email: 'admin@admin.com', password: '123456', password_confirmation: '123456' }])
+Rich.create!([{ email: 'admin@admin.com', password: '123456', password_confirmation: '123456' },
+              { email: 'nicao@nicao.com', password: '123456', password_confirmation: '123456' }])
